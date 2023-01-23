7cb648cd89082a91bf15c0e31411afca3cab8cb4
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-23 16:48:26 +1030

    Clean code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const planets = [
-    { name: 'Mars', isGasPlanet: false },
-    { name: 'Earth', isGasPlanet: false },
-    { name: 'Jupiter', isGasPlanet: true },
-    { name: 'Venus', isGasPlanet: false },
-    { name: 'Neptune', isGasPlanet: true },
-    { name: 'Uranus', isGasPlanet: true }
-  ];
-
   const [showText, setShowText] = useState(true);
 
   const handleClick = (e) => {

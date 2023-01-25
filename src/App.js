48820159193e3c7e2d380b16f83c2901b91b542a
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-25 22:52:19 +1030

    Refactor for new CRUD app

--- src/App.js
+++ src/App.js
@@ -2,26 +2,9 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increaseClick = () => {
-    setCount(count + 1)
-  }
-
-  const decreaseClick = () => {
-    setCount(count - 1)
-  }
-
-  const zero = () => {
-    setCount(0)
-  }
-
   return (
       <div className="App">
-        <button onClick={increaseClick}>Increase</button>
-        <button onClick={decreaseClick}>Decrease</button>
-        <button onClick={zero}>Set to Zero</button>
-        {count}
+
       </div>
   );
 }
@@ -38,4 +21,27 @@ export default App;
 //   <div className="App">
 //     <button style={{color: textColor}} onClick={handleClick}>Show/Hide</button>
 //   </div>
+// );
+
+// const [count, setCount] = useState(0);
+
+// const increaseClick = () => {
+//   setCount(count + 1)
+// }
+
+// const decreaseClick = () => {
+//   setCount(count - 1)
+// }
+
+// const zero = () => {
+//   setCount(0)
+// }
+
+// return (
+//     <div className="App">
+//       <button onClick={increaseClick}>Increase</button>
+//       <button onClick={decreaseClick}>Decrease</button>
+//       <button onClick={zero}>Set to Zero</button>
+//       {count}
+//     </div>
 // );

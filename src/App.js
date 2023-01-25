091d5b220974366d4703bb37a031ab5039879820
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-25 22:40:54 +1030

    Refactor code for next challenge

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [textColor, setTextColor] = useState('red');
-
-  const handleClick = () => {
-    setTextColor(textColor === 'red' ? 'black' : 'red')
-  }
 
   return (
-    <div className="App">
-      <button style={{color: textColor}} onClick={handleClick}>Show/Hide</button>
-    </div>
+      <div className="App">
+
+      </div>
   );
 }
 
 export default App;
+
+// const [textColor, setTextColor] = useState('red');
+
+// const handleClick = () => {
+//   setTextColor(textColor === 'red' ? 'black' : 'red')
+// }
+
+// return (
+//   <div className="App">
+//     <button style={{color: textColor}} onClick={handleClick}>Show/Hide</button>
+//   </div>
+// );

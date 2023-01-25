33349a523f46906427fd9b221756780c09f15209
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-25 22:43:41 +1030

    Set up for challenege

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
       <div className="App">
-
+        <button>Increase</button>
+        <button>Decrease</button>
+        <button>Set to Zero</button>
       </div>
   );
 }

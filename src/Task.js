70931673df1212a6087c7ac822593a11226647c5
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-26 16:29:34 +1030

    Remove useEffect

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
 export const Task = (props) => {
+
+  useEffect(() => {
+    console.log('Component mounted')
+  })
+
   return (
     <div style={{backgroundColor: props.isComplete ? 'green' : 'white'}}>
       <h1>{props.taskName}</h1>

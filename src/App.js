83b88114a61a77ad900ae9c060c199f22d26fc8d
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-27 11:54:01 +1030

    Routes within website and Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from './Pages/Home';
+import { Menu } from './Pages/Menu';
+import { Contact } from './Pages/Contact';
 
 function App() {
   return (
     <div className='App'>
       <Router>
+        <div>NAVBAR
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/menu' element={<Menu />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='*' element={<h1>PAGE NOT FOUND</h1>}/>
         </Routes>
       </Router>
     </div>

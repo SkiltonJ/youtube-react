7211bbf193fcb151ad7a32e400f33ffdb6e80267
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-27 10:58:40 +1030

    Set up using RouterDom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;

c627a9f366a7c25aec5e7c06deb18398a1eaec85
Jackson Jaensch <jackson.jaensch14@gmail.com>
2023-01-31 13:14:42 +1030

    Creating form input using react-hook-form

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -1,9 +1,23 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
 export const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const schema =
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
   return (
     <div>
-      <form>
-
+      <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}} onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="Full Name" {...register("fullName")}></input>
+        <input placeholder="Email" {...register("email")}></input>
+        <input placeholder="Age" {...register("age")}></input>
+        <input type="password" placeholder="Password" {...register("password")}></input>
+        <input type="password" placeholder="Confirm Password" {...register("confirmPassword")}></input>
+        <button type="Submit">Submit</button>
       </form>
     </div>
   )
